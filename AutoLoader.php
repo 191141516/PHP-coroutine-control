7aeac16efe0c33e2458635dc8b34c4ece8f89e5d
kof97 <1048434786@qq.com>
2016-08-31 14:37:15 +0800

    coroutine

--- AutoLoader.php
+++ AutoLoader.php
@@ -1,8 +1,8 @@
 <?php
 
 class AutoLoader {
-    public static function load($class) {
-        $prefix = 'Tsa\\';
+    public static function AutoLoadCoroutine($class) {
+        $prefix = 'Coroutine\\';
 
         $len = strlen($prefix);
         if (strncmp($prefix, $class, $len) !== 0) {
@@ -12,7 +12,25 @@ class AutoLoader {
 
         $class_name = substr($class, $len);
 
-        $file = rtrim(__DIR__, DS) . DS . strtr($class_name, '\\', DS) . '.php';
+        $file = rtrim(__DIR__, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . strtr($class_name, '\\', DIRECTORY_SEPARATOR) . '.php';
+
+        if (is_file($file)) {
+            require $file;
+        }
+    }
+
+    public static function AutoLoadService($class) {
+        $prefix = 'Service\\';
+
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // Another namespace.
+            return;
+        }
+
+        $class_name = substr($class, $len);
+
+        $file = rtrim(__DIR__, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . strtr($class_name, '\\', DIRECTORY_SEPARATOR) . '.php';
 
         if (is_file($file)) {
             require $file;
@@ -20,6 +38,7 @@ class AutoLoader {
     }
 }
 
-spl_autoload_register(array('AutoLoader', 'load'));
+spl_autoload_register(array('AutoLoader', 'AutoLoadCoroutine'));
+spl_autoload_register(array('AutoLoader', 'AutoLoadService'));
 
 // end of script
