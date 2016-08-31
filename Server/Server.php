a965614e5bb09ab037cc7b6e66e4c97d0b812be4
kof97 <1048434786@qq.com>
2016-08-31 15:20:45 +0800

    coroutine

---
+++ Server/Server.php
@@ -0,0 +1,59 @@
+<?php
+
+class Server
+{
+    
+    function __construct(argument)
+    {
+        # code...
+    }
+
+    public function server($port) {
+        echo "Starting server at port $port...\n";
+
+        $socket = stream_socket_server("tcp://localhost:$port", $errNo, $errStr);
+        if (!$socket) throw new Exception($errStr, $errNo);
+
+        stream_set_blocking($socket, 0);
+
+        $socket = new CoSocket($socket);
+        while (true) {
+            yield newTask(
+                handleClient(yield $socket->accept())
+            );
+        }
+    }
+
+    function handleClient($socket) {
+        $data = (yield $socket->read(8192));
+
+        $msg = "Received following request:\n\n$data";
+        $msgLength = strlen($msg);
+
+        $response = <<<RES
+    HTTP/1.1 200 OK\r\n
+    Content-Type: text/plain\r\n
+    Content-Length: $msgLength\r\n
+    Connection: close\r\n
+    \r\n
+    $msg
+    RES;
+
+        yield $socket->write($response);
+        yield $socket->close();
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// end of script
