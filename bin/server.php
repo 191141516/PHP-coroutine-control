1351f564e54c4a20da300cbb7242a39e4e06a341
kof97 <1048434786@qq.com>
2016-09-01 13:09:12 +0800

    coroutine

--- bin/server.php
+++ bin/server.php
@@ -26,24 +26,36 @@ if ($file === 'list') {
 function serverList()
 {
     $confDir = dirname(__dir__) . DIRECTORY_SEPARATOR . 'conf';
+    $list = array();
 
-    $source = fopen($confDir, 'r');
-    $list = readdir();
-    fclose($source);
+    if ($handle = opendir($confDir)) {
+        while (false !== ($file = readdir($handle))) {
+            $path = $confDir . DIRECTORY_SEPARATOR . $file;
 
-    var_dump($confDir);
+            if ($file != "." && $file != ".." && is_file($path)) {
+                array_push($list, $file);
+            }
+        }
+
+        closedir($handle);
+    }
+
+    echo 'Total: ' . count($list) . ' server' . PHP_EOL;
+    foreach ($list as $key => $value) {
+        echo $value . PHP_EOL;
+    }
 }
 
 function serverInfo()
 {
-    echo "***********************************" . PHP_EOL;
-    echo "*  Welcome to use the PHP server  *" . PHP_EOL;
-    echo "***********************************" . PHP_EOL;
-    echo "Welcome to use the PHP server, we can help you to monitor your PHP server!" . PHP_EOL;
-    echo "Command list:" . PHP_EOL;
-    echo "Input server name and command just like: php server.php myServerName" . PHP_EOL;
-    echo "If you want to stop the server, please use: ctrl + c" . PHP_EOL;
-    echo "If you want to know server list that you can input: php server.php list" . PHP_EOL;
+    echo '***********************************' . PHP_EOL;
+    echo '*  Welcome to use the PHP server  *' . PHP_EOL;
+    echo '***********************************' . PHP_EOL;
+    echo 'Welcome to use the PHP server, we can help you to monitor your PHP server!' . PHP_EOL;
+    echo 'Command list:' . PHP_EOL;
+    echo 'Input server name and command just like: php server.php myServerName' . PHP_EOL;
+    echo 'If you want to stop the server, please use: ctrl + c' . PHP_EOL;
+    echo 'If you want to know server list that you can input: php server.php list' . PHP_EOL;
 
     exit;
 }
