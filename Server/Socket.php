d3356bc2d6933cdb68610b83943b0da729c0ad3d
kof97 <1048434786@qq.com>
2016-09-01 11:01:05 +0800

    coroutine

--- Server/Socket.php
+++ Server/Socket.php
@@ -17,11 +17,21 @@ class Socket
      */
     protected $socket;
 
+    /**
+     * Init the socket.
+     *
+     * @param Resource $socket The socket resource.
+     */
     public function __construct($socket)
     {
         $this->socket = $socket;
     }
 
+    /**
+     * Accept the socket stream and return the connection.
+     *
+     * @return Generator
+     */
     public function accept()
     {
         echo "received request from ", stream_socket_get_name($this->socket, 0), "\n";
@@ -30,18 +40,33 @@ class Socket
         yield retval(new Socket(stream_socket_accept($this->socket, 0)));
     }
 
+    /**
+     * Read the socket stream.
+     *
+     * @param int $size The read size.
+     * @return Generator
+     */
     public function read($size)
     {
         yield waitForRead($this->socket);
         yield retval(fread($this->socket, $size));
     }
 
+    /**
+     * Write the socket stream.
+     *
+     * @param string $string The response message.
+     * @return Generator
+     */
     public function write($string)
     {
         yield waitForWrite($this->socket);
         fwrite($this->socket, $string);
     }
 
+    /**
+     * Close the socket stream.
+     */
     public function close()
     {
         @fclose($this->socket);
