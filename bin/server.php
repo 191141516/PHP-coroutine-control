57ed81051af648a2a4d1f43758b7b121485abeb6
kof97 <1048434786@qq.com>
2016-09-01 14:51:01 +0800

    coroutine

--- bin/server.php
+++ bin/server.php
@@ -6,6 +6,8 @@ use Server\Server;
 require './system.functions.php';
 require '../AutoLoader.php';
 
+define('CONF_PATH', dirname(__dir__) . DIRECTORY_SEPARATOR . 'conf');
+
 $file = isset($argv[1]) ? $argv[1] : '';
 
 if ($file === '' || isset($argv[2])) {
@@ -16,6 +18,8 @@ if ($file === 'list') {
     serverList();
 }
 
+readConf($file);
+
 
 // $server = new Server(8000);
 
@@ -23,14 +27,24 @@ if ($file === 'list') {
 // $scheduler->newTask($server->start());
 // $scheduler->run();
 
+function readConf($serv)
+{
+    $ini = CONF_PATH . DIRECTORY_SEPARATOR . $serv . '.ini';
+    if (!is_file($ini)) {
+        echo "This server named $serv is not exist" . PHP_EOL;
+        exit;
+    }
+
+
+}
+
 function serverList()
 {
-    $confDir = dirname(__dir__) . DIRECTORY_SEPARATOR . 'conf';
     $list = array();
 
-    if ($handle = opendir($confDir)) {
+    if ($handle = opendir(CONF_PATH)) {
         while (false !== ($file = readdir($handle))) {
-            $path = $confDir . DIRECTORY_SEPARATOR . $file;
+            $path = CONF_PATH . DIRECTORY_SEPARATOR . $file;
 
             if ($file != "." && $file != ".." && is_file($path)) {
                 array_push($list, $file);
@@ -44,14 +58,19 @@ function serverList()
     foreach ($list as $key => $value) {
         echo $value . PHP_EOL;
     }
+
+    exit;
 }
 
 function serverInfo()
 {
+    echo PHP_EOL;
     echo '***********************************' . PHP_EOL;
     echo '*  Welcome to use the PHP server  *' . PHP_EOL;
     echo '***********************************' . PHP_EOL;
+    echo PHP_EOL;
     echo 'Welcome to use the PHP server, we can help you to monitor your PHP server!' . PHP_EOL;
+    echo PHP_EOL;
     echo 'Command list:' . PHP_EOL;
     echo 'Input server name and command just like: php server.php myServerName' . PHP_EOL;
     echo 'If you want to stop the server, please use: ctrl + c' . PHP_EOL;
